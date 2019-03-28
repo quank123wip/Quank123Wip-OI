ee1728de72fa838dcbc51b976793ea66f54169c1
Quank <quank123wip@outlook.com>
2019-03-28 17:54:11 +0800

    orz

--- Luogu/Codes/P3954.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    double a, b, c, ans;
-    cin >> a >> b >> c;
-    ans = a * 0.2 + b * 0.3 + c * 0.5;
-    cout << ans << endl;
-}
--- SimTest/2018.7.10/Quank/exhibit.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-const int MAXN=1000001;
-const int MAXM=200001;
-int main(){
-    //freopen("exhibit.cpp","r",stdin);
-    //freopen("exhibit.cpp","w",stdout);
-    
-}
